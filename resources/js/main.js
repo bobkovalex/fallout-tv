e885b4f891d274bd5f77c4e728340e7aa3ac2ef1
Alex Bobkov <lilalex85@gmail.com>
2024-05-19 10:32:48 +0100

    added f4 specials

--- resources/js/main.js
+++ resources/js/main.js
@@ -2,11 +2,31 @@ function openFullscreen() {
     document.documentElement.requestFullscreen();
 }
 
-$(document).bind('webkitfullscreenchange mozfullscreenchange fullscreenchange', function(e) {
+$(document).ready(function () {
+    var counter = 1;
+    const videoPlayer = $('#video-player');
+    const videoLoading = $('#video-loading');
+
+    videoPlayer.on('ended', function () {
+        if (counter >= 8) {
+            counter = 1;
+        }
+        videoPlayer.hide();
+        videoLoading.show();
+        setTimeout(function () {
+            videoPlayer.show();
+            videoPlayer.attr('src', `resources/video/f4_${counter++}.mp4`);
+        }, 10000);
+        
+    });
+});
+
+
+$(document).bind('webkitfullscreenchange mozfullscreenchange fullscreenchange', function (e) {
     var state = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
-    if(state){
+    if (state) {
         $('#btn').hide();
-    }else{
+    } else {
         $('#btn').show();
-    }   
+    }
 });
